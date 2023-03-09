4cf02d84d94cfa580b6b8ab1c5da5e69d0096565
Hugo-galaxy <hugomonteiroguedes@gmail.com>
2023-03-09 17:38:02 -0300

    mais exercicios feitos

---
+++ src/exercicio17.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class exercicio17 {
+    public static void main (String [] args){
+        Scanner scan =new Scanner(System.in);
+        System.out.println("digite o valor do primeiro lado do triangulo");
+        double lado1= scan.nextDouble();
+        System.out.println("digite o valor do segundo lado do triangulo");
+        double lado2= scan.nextDouble();
+        System.out.println("digite o valor do terceiro lado do triangulo");
+        double lado3= scan.nextDouble();
+
+
+        if (lado1+lado2>lado3){
+            System.out.println("é um triangulo");
+            if (lado1==lado2 && lado2==lado3 && lado1==lado3){
+                System.out.println("é um triangulo equilatero");
+            } else if (lado1==lado2 || lado2==lado3 || lado1==lado3){
+                System.out.println("é um triangulo isósceles");
+            }else {
+                System.out.println("é um triangulo escaleno");
+            }
+        } else if (lado2+lado3>lado1){
+            System.out.println("é triangulo");
+            if (lado1==lado2 && lado2==lado3 && lado1==lado3){
+                System.out.println("é um triangulo equilatero");
+            } else if (lado1==lado2 || lado2==lado3 || lado1==lado3){
+                System.out.println("é um triangulo isósceles");
+            }else {
+                System.out.println("é um triangulo escaleno");
+            }
+        } else if (lado1+lado3>lado2){
+            System.out.println("é triangulo");
+            if (lado1==lado2 && lado2==lado3 && lado1==lado3){
+                System.out.println("é um triangulo equilatero");
+            } else if (lado1==lado2 || lado2==lado3 || lado1==lado3){
+                System.out.println("é um triangulo isósceles");
+            }else {
+                System.out.println("é um triangulo escaleno");
+            }
+        }else {
+            System.out.println("não é um triangulo");
+        }
+    }
+}
---
+++ src/exercicio8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class exercicio8 {
+    public static void main (String[] args){
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("digite o primeiro numero:");
+        double numero1= scan.nextDouble();
+        System.out.println("digite o segundo numero:");
+        double numero2= scan.nextDouble();
+
+        if (numero1>numero2){
+            System.out.printf("%f é maior",numero1);
+        }else {
+            System.out.printf("%.2f é maior",numero2);
+        }
+    }
+}
