d1fe7a1d3f38be51c306dd51ffbd8a265270021e
Hugo-galaxy <hugomonteiroguedes@gmail.com>
2023-03-22 18:32:07 -0300

    exercicios com arrays

--- src/exercicio27.java
+++ src/exercicio27.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class exercicio27 {
     public static void main (String [] args){
+        int i,num;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("entre com o numero fatorial");
+        num = scan.nextInt();
+        int fatorial=1;
 
+        for (i=num;i>0;i--){
+            fatorial=fatorial*i;
+            System.out.printf("%d \n",i);
+        }
+        System.out.printf("\n fatorial é: %d ",fatorial);
     }
 }
